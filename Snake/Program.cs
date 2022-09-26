a7b9ff6f1877c1f2cebb65c8fd93257e63f24517
ESCG\535439 <535439@chrome.escg.ac.uk>
2022-09-26 14:25:22 +0100

    Add project files.

---
+++ Snake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //screen
+        int width = 3;
+        int height = 5;
+
+        //snake
+        char snake_chr = 's';
+        int snake_length = 1;
+
+        //food 
+        char food_chr = 'f';
+        int food_count = 1;
+        
+        for(int y=0; y<height; y++)
+        {
+            Console.Write("|");
+            for(int x=0; x<width; x++)
+            {
+                Console.Write("x");
+            }
+            Console.WriteLine("|\n");
+        }
+        for(int i=0; i<width*2; i++) 
+        {
+            Console.Write("- ");
+        }
+        
+    }
+}
